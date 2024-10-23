467a791643f00ab94ab372197e6358d0c86f1caa
Carlos Trindade <carlos.trindade@ufms.br>
2024-10-22 22:11:30 -0400

    Iniciando o desenvolvimento das páginas de login e cadastro

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Signup() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -11,7 +11,7 @@ export default function Home() {
             className="mx-auto h-10 w-auto"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Entre em sua conta  
+            Crie sua conta
           </h2>
         </div>
 
@@ -38,11 +38,24 @@ export default function Home() {
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                   Senha
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Esqueceu sua senha?
-                  </a>
-                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Confirme sua senha
+                </label>
               </div>
               <div className="mt-2">
                 <input
@@ -61,15 +74,15 @@ export default function Home() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Entrar
+                Criar conta
               </button>
             </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Não possui uma conta?{' '}
-            <Link href="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Criar uma conta
+            Já possui uma conta?{' '}
+            <Link href="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Entre
             </Link>
           </p>
         </div>
