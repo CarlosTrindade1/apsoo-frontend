a816375b51796a037c325a333ffa2b8bda4d9ff0
carlos.trindade <carlos.trindade@ufms.br>
2024-11-08 20:41:11 -0400

    Atualizando a página de cadastro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,32 @@
 import { Button, Checkbox, Flowbite, Label, TextInput } from 'flowbite-react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <Flowbite>
         <div className="flex items-center justify-center h-screen dark:bg-gray-900">
-          <form className="flex max-w-lg grow flex-col gap-4">
+          <form className="flex max-w-md grow flex-col gap-4">
             <div>
               <div className="mb-2 block">
-                <Label htmlFor="email1" value="E-mail" />
+                <Label htmlFor="email1" value="E-mail"/>
               </div>
-              <TextInput id="email1" type="email" placeholder="name@email.com" required />
+              <TextInput id="email1" type="email" placeholder="name@email.com" required shadow/>
             </div>
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="password1" value="Senha" />
               </div>
-              <TextInput id="password1" type="password" required />
+              <TextInput id="password1" type="password" required shadow/>
             </div>
             <div className="flex items-center gap-2">
-              <Checkbox id="remember" />
+              <Checkbox id="remember"/>
               <Label htmlFor="remember">Lembrar de mim</Label>
             </div>
             <Button type="submit">Entrar</Button>
+            <div className='flex items-center gap-2'>
+              <Label>Não tem uma conta? <Link href="/signup" className='text-sky-700'>Cadastre-se</Link></Label>
+              <Label><Link href="/dashboard">Dashboard</Link></Label>
+            </div>
           </form>
         </div>
     </Flowbite>
